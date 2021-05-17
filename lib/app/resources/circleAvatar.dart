c4e4a4302bd7104603bf1757330750369afafb19
kleinvanessa <vanessalopesklein@gmail.com>
2021-05-17 17:00:54 -0300

    add image in circular avatar

--- lib/app/resources/circleAvatar.dart
+++ lib/app/resources/circleAvatar.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:shared_projects/app/ui/profile/profile.dart';
-//import 'package:flutter_modular/flutter_modular.dart';
 
 class CircularAvatar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      //backgroundImage: AssetImage('assets/img/profile.jpg'),
       backgroundColor: Colors.white,
       radius: 42.0,
       child: Material(
-        child: _typeOfCircleAvatar(context),
+        elevation: 4.0,
         shape: CircleBorder(),
+        clipBehavior: Clip.hardEdge,
+        color: Colors.transparent,
+        child: Ink.image(
+          image: AssetImage('assets/img/profile.jpg'),
+          fit: BoxFit.cover,
+          width: 120.0,
+          height: 120.0,
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => ProfilePage()));
+            },
+          ),
+        ),
       ),
     );
   }
-
-  Widget _typeOfCircleAvatar(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (BuildContext context) => ProfilePage()));
-      },
-    );
-  }
 }
-//AssetImage('assets/img/profile.jpg'),
