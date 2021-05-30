d1663a84ce201ab93b7cf0b39ca30db89ec8f8c8
kleinvanessa <vanessalopesklein@gmail.com>
2021-05-30 20:35:45 -0300

    add User Controller and change launch URL from API

---
+++ web/Web.shared_projects/Web.shared_projects/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.shared_projects.Data;
+using Web.shared_projects.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web.shared_projects.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase {
+        // GET: api/<UserController>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id) {
+            var user = new User { FirstName = "Vanessa", LastName = "Klein", Email = "vanessa@gmail.com" };
+            using (var context = new SharedProjContext()) {
+                context.User.Add(user);
+                context.SaveChanges();
+            }
+            return Ok();
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] string value) {
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
