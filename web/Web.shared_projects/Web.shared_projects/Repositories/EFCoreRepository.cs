fe8f670175e3a2a588a3201fde382c9a9b716a0e
kleinvanessa <vanessalopesklein@gmail.com>
2021-05-31 14:35:01 -0300

    add Interface

---
+++ web/Web.shared_projects/Web.shared_projects/Repositories/EFCoreRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.shared_projects.Data;
+
+namespace Web.shared_projects.Repositories {
+    public class EFCoreRepository : IEFCoreRepository {
+        private readonly SharedProjContext _context;
+        public EFCoreRepository(SharedProjContext context) {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class {
+            _context.Add(entity);
+        }
+
+        public void Update<T>(T entity) where T : class {
+            _context.Update(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class {
+            _context.Remove(entity);
+        }
+
+        public async Task<bool> SaveChangeAsync() {
+            return (await _context.SaveChangesAsync()) > 0;
+        }
+
+        
+    }
+}
---
+++ web/Web.shared_projects/Web.shared_projects/Repositories/IEFCoreRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.shared_projects.Repositories {
+    public interface IEFCoreRepository {
+        void Add<T>(T entity) where T : class; // assinatura generica que aceita qualquer tipo como parametro / T = tipo
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+
+        Task<bool> SaveChangeAsync();
+    }
+}
